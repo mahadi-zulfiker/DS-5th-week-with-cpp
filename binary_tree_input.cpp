d2ef004fec0a52ce67d3ad7837591d5a4dbb3ff9
mahade <mahade.adib45@gmail.com>
2025-01-15 20:47:27 +0600

    binary tree input

---
+++ binary_tree_input.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }     
+};
+
+Node* input_tree(){
+    int val;
+    cin >> val;
+    Node* root = new Node(val);
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        Node* p= q.front();
+        q.pop();
+
+        int l,r;
+        cin >> l >> r;
+        Node* myLeft, *myRight;
+        if(l == -1) myLeft = NULL;
+        else myLeft = new Node(l);
+        if(r == -1) myRight = NULL;
+        else myRight = new Node(r);
+
+        p->left = myLeft;
+        p->right = myRight;
+
+        if(p->left)
+            q.push(p->left);
+        if(p->right)
+            q.push(p->right);
+    }
+    return root;
+}
+
+void level_order(Node* root){
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        Node* f = q.front();
+        q.pop();
+        cout<<f->val<<" ";
+        if(f->left != NULL)
+            q.push(f->left);
+        if(f->right != NULL)
+            q.push(f->right);
+    }
+}
+
+int main(){
+    Node* root = input_tree();
+    level_order(root);
+    
+    return 0;
+}
